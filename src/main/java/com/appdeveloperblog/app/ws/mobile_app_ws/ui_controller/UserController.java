7596b3d4c483704e70a1731c3bacd79b3a1ffddf
Kousthubha GK <kousthubhagk@gmail.com>
2025-01-28 17:53:18 +0530

    added ui controller

---
+++ src/main/java/com/appdeveloperblog/app/ws/mobile_app_ws/ui_controller/UserController.java
@@ -0,0 +1,27 @@
+package com.appdeveloperblog.app.ws.mobile_app_ws.ui_controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+    @GetMapping
+    public String getUser(){
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(){
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser(){
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser(){
+        return "delete user was called";
+    }
+}
